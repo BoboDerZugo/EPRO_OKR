7ca0534df8aee41fd79d21b1e1c19b9cd94c1a5a
Felix Polte <felix.polte@th-bingen.de>
2024-01-22 15:11:41 +0100

    KeyResultController/Service

---
+++ src/main/java/com/example/controller/KeyResultController.java
@@ -0,0 +1,56 @@
+package com.example.controller;
+
+import com.example.service.KeyResultService;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import com.example.model.*;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.lang.NonNull;
+
+//CRUD operations for KeyResults
+@RestController
+@RequestMapping("/keyresult")
+public class KeyResultController {
+
+    @Autowired
+    private KeyResultService keyResultService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<KeyResult> getAllKeyResults(){
+        return keyResultService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public KeyResult getKeyResultById(@PathVariable("id") UUID id){
+        Optional<KeyResult> keyResult = keyResultService.findById(id);
+        return keyResult.get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public KeyResult createKeyResult(@RequestBody  @NonNull KeyResult keyResult){
+        return keyResultService.insert(keyResult);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public KeyResult updateKeyResult(@PathVariable("id") UUID id, @RequestBody KeyResult keyResult){
+        Optional<KeyResult> keyResultToUpdate = keyResultService.updateOne(id, keyResult);
+        return keyResultToUpdate.get();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public KeyResult deleteKeyResult(@PathVariable("id") Long id){
+        Optional<KeyResult> keyResultToDelete = keyResultService.delete(id);
+        return keyResultToDelete.get();
+    }
+}
+
--- src/main/java/com/example/service/KeyResultService.java
+++ src/main/java/com/example/service/KeyResultService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.example.model.KeyResult;
 import com.example.model.Unit;
 import com.example.model.User;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
@@ -11,6 +12,8 @@ import java.util.UUID;
 public interface KeyResultService extends MongoRepository<KeyResult, UUID> {
     Optional<KeyResult> findByOwnerEquals(User user);
     Optional<KeyResult> findByContributingUnitsContains(Unit unit);
+    Optional<KeyResult> updateOne(UUID id, KeyResult keyResult);
+    Optional<KeyResult> delete(Long id);
 
 
 }
