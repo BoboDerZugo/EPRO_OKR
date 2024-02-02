4b0e1ecbbfc17aa874fa388b65fc936a8f85c002
martin.nebeling <martin.nebeling@th-bingen.de>
2024-02-02 11:42:27 +0100

    Fixed Tests onto Junit Jupiter

--- src/test/java/com/example/service/BusinessUnitServiceTest.java
+++ src/test/java/com/example/service/BusinessUnitServiceTest.java
@@ -2,7 +2,7 @@ package com.example.service;
 
 import com.example.model.*;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
