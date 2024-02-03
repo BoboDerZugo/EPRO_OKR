3bf37839c0e1f52fa664d0cacb2d726b3179fcaa
Felix Polte <felix.polte@th-bingen.de>
2024-02-03 11:53:29 +0100

    Add POST endpoint to Controller.java and configure MongoDB properties in application.properties

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,12 @@ public class Controller {
     public String helloWorld(){
         System.out.println("Hello World!");
         return "Hello World";
+    }
+
+    @PostMapping
+    public ResponseEntity<String> helloWorldPost(){
+        System.out.println("j");
+        return  ResponseEntity.ok("Hello World");
     }
 
     
