e70bf8bb9563b39db36ea5594e01ceca3869a3d6
Felix Polte <felix.polte@th-bingen.de>
2024-02-15 11:49:28 +0100

    Authentificate with Company Users

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.example.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -20,26 +17,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig{
 
-    // @Bean
-    // @ConditionalOnMissingBean(UserDetailsService.class)
-    // InMemoryUserDetailsManager inMemoryUserDetailsManager() { 
-    //     String generatedPassword = "password";
-    //     return new InMemoryUserDetailsManager(User.withUsername("user")
-    //             .password(generatedPassword).roles("USER").build());
-    // }
-
-    // @Bean
-    // @ConditionalOnMissingBean(AuthenticationEventPublisher.class)
-    // DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegate) { 
-    //     return new DefaultAuthenticationEventPublisher(delegate);
-    // }
 
     @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
+		http.csrf().disable()
 			.authorizeHttpRequests((authorize) -> authorize
 				.requestMatchers("/example").permitAll()
-                .anyRequest().authenticated()
+                .requestMatchers("/user").permitAll()
+                .requestMatchers("/company").hasRole("CO_ADMIN")
+                .anyRequest().permitAll()
 			)
 			.httpBasic(Customizer.withDefaults());
 
@@ -47,14 +33,36 @@ public class SecurityConfig{
 	}
 
     @Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-			.username("user")
-			.password("password")
-			.roles("USER")
-			.build();
+	public AuthenticationManager authenticationManager(
+			UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
 
-		return new InMemoryUserDetailsManager(userDetails);
+		return new ProviderManager(authenticationProvider);
 	}
+
+    // @Bean
+	// public MyUserDetailsService customUserDetailsService() {
+    //     return new MyUserDetailsService();
+	// }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return charSequence.toString().equals(s);
+            }
+        };
+    }
+
+
 
 }
