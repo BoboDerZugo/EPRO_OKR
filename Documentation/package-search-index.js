44841c4c0031f18a3c9be65f6ff9dbd9434d8d2b
martin.nebeling <martin.nebeling@th-bingen.de>
2024-02-25 11:15:10 +0100

    Javadoc generated by Intellij

---
+++ Documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"com.example"},{"l":"com.example.config"},{"l":"com.example.controller"},{"l":"com.example.model"},{"l":"com.example.service"}];updateSearchResults();
